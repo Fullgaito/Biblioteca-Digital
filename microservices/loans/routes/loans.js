a426194c6d16dd66da3b92b41262cc17a4b30309
Fullgaito <gaitansergioalejandrgmail.com>
2026-04-01 11:31:17 -0500

    feat:relacion con ms-books para disminuir o aumentar cuando se hace un prestamo

--- microservices/loans/routes/loans.js
+++ microservices/loans/routes/loans.js
@@ -2,16 +2,71 @@ const express = require('express') // Rutas para manejar los préstamos
 const router = express.Router()
 const Loan = require('../models/Loan') // Modelo de préstamo
 
-// ── POST /loans ───────────────────────────────────────────────
-router.post('/', async (req, res) => { // Registrar un nuevo préstamo
-    try {
-        const { usuario_id, libro_id } = req.body
+const authInternal = function (req, res, next) {
+    const apiKey = req.headers['x-internal-api-key'];
+    const internalKey = process.env.INTERNAL_API_KEY;
 
-        if (!usuario_id || !libro_id) {
+    if (!apiKey || apiKey !== internalKey) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    next();
+};
+
+// ── POST /loans ───────────────────────────────────────────────
+router.post('/', authInternal, async (req, res) => { // Registrar un nuevo préstamo
+    let stockDecremented = false;
+    try {
+        const { user_id, book_id } = req.body
+
+        if (!user_id || !book_id) {
             return res.status(400).json({ error: 'Missing fields' })
         }
+        const existingLoan = await Loan.findOne({
+            user_id,
+            book_id,
+            status: 'active'
+        });
 
-        const newLoan = new Loan({ usuario_id, libro_id })
+        if (existingLoan) {
+            return res.status(400).json({ error: 'User already has this book' });
+        }
+        
+        //Llamar a flask
+        const response = await fetch(`${process.env.FLASK_URL}/books/${book_id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Internal-API-Key': process.env.INTERNAL_API_KEY
+            },
+            timeout: 5000 // 5 segundos de timeout
+        });
+
+        if (!response.ok) {
+            return res.status(404).json({ error: 'Book not found' });
+        }
+
+        const book = await response.json();
+
+        if (!book.quantity  || book.quantity <= 0) {
+            return res.status(400).json({ error: 'Not Stock available' })
+        }
+
+        // 1. descontar stock
+        const decrementResponse = await fetch(`${process.env.FLASK_URL}/books/${book_id}/decrement`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Internal-API-Key': process.env.INTERNAL_API_KEY
+            }
+        });
+
+        if (!decrementResponse.ok) {
+            return res.status(500).json({ error: 'Error updating stock' });
+        }
+        stockDecremented = true;
+        
+
+        const newLoan = new Loan({ user_id, book_id })
         await newLoan.save()
 
         return res.status(201).json({
@@ -20,12 +75,20 @@ router.post('/', async (req, res) => { // Registrar un nuevo préstamo
         })
 
     } catch (error) {
+        // 🔥 rollback (devolver stock)
+        await fetch(`${process.env.FLASK_URL}/books/${book_id}/increment`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Internal-API-Key': process.env.INTERNAL_API_KEY
+            }
+        });
         return res.status(500).json({ error: error.message })
     }
 })
 
 // ── GET /loans ───────────────────────────────────────────────
-router.get('/', async (req, res) => { // Obtener todos los préstamos
+router.get('/',authInternal, async (req, res) => { // Obtener todos los préstamos
     try {
         const loans = await Loan.find()
         return res.status(200).json(loans)
@@ -35,35 +98,49 @@ router.get('/', async (req, res) => { // Obtener todos los préstamos
 })
 
 // ── PUT /loans/:id/return ────────────────────────────────────
-router.put('/:id/return', async (req, res) => { // Devolver un libro
+router.put('/:id/return', authInternal, async (req, res) => { // Devolver un libro
     try {
-        const loan = await Loan.findByIdAndUpdate(
-            req.params.id,
-            {
-                estado: 'devuelto',
-                fecha_devolucion: new Date()
-            },
-            { new: true }
-        )
+        const loan = await Loan.findById(req.params.id);
 
         if (!loan) {
             return res.status(404).json({ error: 'Loan not found' })
         }
 
+        if (loan.status === 'returned') {
+            return res.status(400).json({ error: 'Loan already returned' });
+        }
+        // devolver stock
+        const incrementResponse = await fetch(`${process.env.FLASK_URL}/books/${loan.book_id}/increment`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Internal-API-Key': process.env.INTERNAL_API_KEY
+            }
+        });
+
+        if (!incrementResponse.ok) {
+            return res.status(500).json({ error: 'Error updating stock' });
+        }
+
+        loan.status = 'returned';
+        loan.return_date = new Date();
+        await loan.save();
+
         return res.status(200).json({
             message: 'Book returned successfully',
             loan
         })
+        
 
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
 })
 
-// ── GET /loans/usuario/:usuario_id ───────────────────────────
-router.get('/usuario/:usuario_id', async (req, res) => { // Obtener préstamos por usuario
+// ── GET /loans/usuario/:user_id ───────────────────────────
+router.get('/usuario/:user_id', authInternal, async (req, res) => { // Obtener préstamos por usuario
     try {
-        const loans = await Loan.find({ usuario_id: req.params.usuario_id })
+        const loans = await Loan.find({ user_id: req.params.user_id })
 
         if (!loans.length) {
             return res.status(404).json({ error: 'No loans found for this user' })
@@ -77,9 +154,9 @@ router.get('/usuario/:usuario_id', async (req, res) => { // Obtener préstamos p
 })
 
 // ── GET /loans/activos ───────────────────────────────────────
-router.get('/activos', async (req, res) => { // Obtener préstamos activos
+router.get('/activos', authInternal, async (req, res) => { // Obtener préstamos activos
     try {
-        const loans = await Loan.find({ estado: 'activo' })
+        const loans = await Loan.find({ status: 'active' })
         return res.status(200).json(loans)
     } catch (error) {
         return res.status(500).json({ error: error.message })
